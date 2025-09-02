ca40ab7c6314e2f2c283e758f0ac5bf609b1708c
Victor Caricatte <victorleniwys@gmail.com>
2025-09-02 07:01:17 -0300

    panvita.py

--- panvita.py
+++ panvita.py
@@ -292,13 +292,13 @@ class DependencyManager:
 
             if PanViTaConfig.is_windows():
                 diamond_file = FileHandler.safe_download(
-                    "http://github.com/bbuchfink/diamond/releases/download/v2.1.13/diamond-windows.zip")
+                    "https://github.com/bbuchfink/diamond/releases/download/v2.1.13/diamond-windows.zip")
                 with zipfile.ZipFile(diamond_file, 'r') as zip_ref:
                     zip_ref.extractall('.')
                 diamond_exe = "diamond.exe"
             else:
                 diamond_file = FileHandler.safe_download(
-                    "http://github.com/bbuchfink/diamond/releases/download/v2.1.13/diamond-linux64.tar.gz")
+                    "https://github.com/bbuchfink/diamond/releases/download/v2.1.13/diamond-linux64.tar.gz")
                 FileHandler.extract_tar_file(diamond_file)
                 diamond_exe = "diamond"
                 if not PanViTaConfig.is_windows():
